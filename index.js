f5709e928d8518ef752244a81f653a1bfbaced69
Sachin Mathur <samathur175@gmail.com>
2022-10-11 23:47:16 +0530

    Added Funconality to add tasks, edit tasks and delte tasks; using Javascript. Need some minor twicking to complete the project, will complete that in next Commit.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+const inputTaskEl = document.querySelector("#input-field");
+const addBtnEl = document.querySelector("#add-btn");
 
+addBtnEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (inputTaskEl.value === "") {
+    alert("Add Your Task First");
+  } else {
+    let newTaskDiv = document.createElement("div");
+    newTaskDiv.classList.add("task-div");
+
+    let newTaskEl = document.createElement("input");
+    newTaskEl.setAttribute("readonly", true);
+    newTaskEl.classList.add("new-task-el");
+    newTaskEl.value = inputTaskEl.value;
+
+    let editBtnEl = document.createElement("button");
+    editBtnEl.classList.add("edit-btn");
+    editBtnEl.innerHTML = `Edit`;
+
+    editBtnEl.addEventListener("click", () => {
+      if ((editBtnEl.innerHTML = "Edit")) {
+        newTaskEl.removeAttribute("readonly");
+        editBtnEl.innerHTML = `Save`;
+      } else {
+        newTaskEl.setAttribute("readonly", true);
+      }
+    });
+
+    let delBtnEl = document.createElement("button");
+    delBtnEl.classList.add("del-btn");
+    delBtnEl.innerHTML = `Delete`;
+
+    delBtnEl.addEventListener("click", () => {
+      document.body.removeChild(newTaskDiv);
+    });
+
+    newTaskDiv.appendChild(newTaskEl);
+    newTaskDiv.appendChild(editBtnEl);
+    newTaskDiv.appendChild(delBtnEl);
+    document.body.appendChild(newTaskDiv);
+  }
+
+  inputTaskEl.value = ``;
+});
